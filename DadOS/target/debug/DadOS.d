9120249b73ef5ae44c17511f2a6667d2b10cde96
HadsonRamalho <hadsonramalho@gmail.com>
2024-09-25 01:07:45 -0300

    Adicionando 'no_std', abort on panic, core

---
+++ DadOS/target/debug/DadOS.d
@@ -0,0 +1,1 @@
+C:\Users\Hadson\Documents\DadOS\DadOS\target\debug\DadOS.exe: C:\Users\Hadson\Documents\DadOS\DadOS\src\main.rs
---
+++ DadOS/target/debug/deps/DadOS.d
@@ -0,0 +1,5 @@
+C:\Users\Hadson\Documents\DadOS\DadOS\target\debug\deps\DadOS.exe: src/main.rs
+
+C:\Users\Hadson\Documents\DadOS\DadOS\target\debug\deps\DadOS.d: src/main.rs
+
+src/main.rs:
